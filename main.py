89dc1a7e323fb3f3a10191447461d9d197985aba
magistraapta <magsitraagis@gmail.com>
2024-11-11 18:09:30 +0800

    update router on main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from user.routes import user_router
+from product.routes import product_router
+from review.routes import review_router
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(product_router)
+app.include_router(review_router)
