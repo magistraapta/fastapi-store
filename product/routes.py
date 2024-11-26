c1d90e6736a7c7e87fd93240eefc5a6215692574
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2024-11-26 21:03:50 +0800

    add error handling

--- product/routes.py
+++ product/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from product.schema import ProductResponse, ProductCreate
 from db import get_db
@@ -13,12 +13,24 @@ product_service = ProductServices(product_repository=product_repository)
 
 @product_router.get("/", response_model=List[ProductResponse])
 def get_all_products(db: Session = Depends(get_db)):
-    return product_service.get_all_products(db=db)
+    try:
+        
+        return product_service.get_all_products(db=db)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error getting all products: {e}")
 
 @product_router.get("/{product_id}", response_model=ProductResponse)
 def get_product(product_id: int, db: Session = Depends(get_db)):
-    return product_service.get_products(db=db, product_id=product_id)
+    try:
+        
+        return product_service.get_products(db=db, product_id=product_id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error getting product: {e}")
 
 @product_router.post("/", response_model=ProductResponse)
 def create_user(product_create: ProductCreate, db: Session = Depends(get_db)):
-    return product_service.create_product(db=db, product=product_create)
+    try:
+        
+        return product_service.create_product(db=db, product=product_create)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error creating product: {e}")
