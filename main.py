80934bf7bac5cfb700282124b22e11012f9387a9
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2024-12-09 16:42:02 +0800

    fix: cors configuration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from user.routes import user_router
 from product.routes import product_router
@@ -9,6 +10,14 @@ from auth.routes import auth_router
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name='static')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Update with your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(user_router)
 app.include_router(product_router)
