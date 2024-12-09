6487cc5acc2fc3eb051218fab6f9d205f678e1df
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2024-12-09 14:29:38 +0800

    fix: use .env

--- db.py
+++ db.py
@@ -1,8 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL="postgresql://postgres:postgres@localhost/python_api"
+load_dotenv()
+
+DATABASE_USERNAME=os.getenv("DATABASE_USERNAME")
+DATABASE_PASSWORD=os.getenv("DATABASE_PASSWORD")
+DATABASE_HOST=os.getenv("DATABASE_HOST")
+DATABASE_PORT=os.getenv("DATABASE_PORT")
+DATABASE_NAME=os.getenv("DATABASE_NAME")
+
+DATABASE_URL=f"postgresql://{DATABASE_USERNAME}:{DATABASE_NAME}@{DATABASE_HOST}/{DATABASE_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
