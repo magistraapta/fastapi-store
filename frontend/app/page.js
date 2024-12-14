4cdb6bff0d096d5d08bdbc5ed79da347b43e7906
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2024-12-14 14:49:52 +0800

    fix: product card

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import Card from "./components/Card";
 
 export default async function Home() {
   const res = await fetch('http://localhost:8000/v1/products')
@@ -6,12 +7,9 @@ export default async function Home() {
   return (
     <div>
       <Navbar/>
-      <div className="grid grid-cols-4 mt-4 gap-y-4">
+      <div className="grid grid-cols-4 max-lg:grid-cols-2 mt-4 gap-y-4">
         {data.map((product) => (
-          <div className="border rounded-xl shadow-md w-[200px]">
-            <p key={product.id}>{product.name}</p>
-            <p>{product.price}</p>
-          </div>
+          <Card id={product.id} name={product.name} price={product.price} image_url={product.image_url}/>
         ))}
       </div>
     </div>
