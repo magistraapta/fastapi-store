55108c5de473270aa58f1dc290a434f3d342b1d2
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2024-12-14 14:50:01 +0800

    fix: card component

--- frontend/app/components/Card.js
+++ frontend/app/components/Card.js
@@ -1,7 +1,22 @@
-export default function Card({title, name, image, description, price, stock}) {
+import Image from "next/image";
+
+export default function Card({id, image_url, name, price}) {
     return (
         <div>
-            
+            <div className="border rounded-xl shadow-md w-[300px] p-4">
+                <div key={id}>
+                    <Image
+                        src={image_url}
+                        width={400}
+                        height={200}
+                        alt="product-image"
+                    />
+                    <div>
+                    <h4 className="text-3xl font-semibold">{name}</h4>
+                    <p>Rp.{price}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
