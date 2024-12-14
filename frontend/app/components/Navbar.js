466b116ead03bd8fc1de80ee6a658295c958a23d
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2024-12-14 14:49:28 +0800

    fix: navbar

--- frontend/app/components/Navbar.js
+++ frontend/app/components/Navbar.js
@@ -17,33 +17,26 @@ export default function Navbar() {
         setIsLoggedIn(false);
     };
 
-    
-
+    const navbarList = ["All", "Gadget", "Shoes"]
     return (
-        <nav className="grid grid-cols-2">
-            <div>
-                <h1 className="font-bold text-4xl">
-                    FastStore
-                </h1>
+        <nav className="grid grid-cols-3 items-center">
+            <div className=" flex w-30 items-center gap-4">
+
+                <p className=" text-sm font-bold">FastStore</p>
+
+                <div className="max-lg:hidden flex gap-4">
+                    {navbarList.map((item, index) => (
+                        <p className=" text-gray-600 text-sm" key={index}>{item}</p>
+                    ))}
+                </div>
             </div>
-            <div className="flex justify-end space-x-4 items-center">
-                {isLoggedIn ? (
-                    <>
-                        <a href="/admin" className="text-blue-500 hover:underline">
-                            Admin
-                        </a>
-                        <button
-                            onClick={handleLogout}
-                            className="text-red-500 hover:underline"
-                        >
-                            Logout
-                        </button>
-                    </>
-                ) : (
-                    <a href="/login" className="text-blue-500 hover:underline">
-                        Login
-                    </a>
-                )}
+            
+            <div>
+                <input type="text" placeholder="input for search" className="border border-gray-300 rounded-lg py-2 px-4 w-full" />
+            </div>
+
+            <div className="flex justify-end">
+                <button className="text-gray-600 text-sm border border-blue-600 rounded-lg p-2">Cart</button>
             </div>
         </nav>
     );
