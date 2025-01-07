136b0e0e23a82594444cfecd297dd39eba4c6138
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-01-07 12:25:48 +0800

    fix: change product response to json

--- product/routes.py
+++ product/routes.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
-from product.schema import ProductResponse, ProductCreate
+from product.schema import ProductResponse, ProductCreate, ProductListResposne
 from db import get_db
 from product.repository import ProductRepository
 from product.services import ProductServices
@@ -11,21 +12,37 @@ product_router = APIRouter(prefix="/v1/products", tags=["products"])
 product_repository = ProductRepository()
 product_service = ProductServices(product_repository=product_repository)
 
-@product_router.get("/", response_model=List[ProductResponse])
+@product_router.get("/", response_model=ProductListResposne)
 def get_all_products(db: Session = Depends(get_db)):
     try:
+        products = product_service.get_all_products(db)
         
-        return product_service.get_all_products(db=db)
+        return {
+            "message": "Success retrieved all products",
+            "products": products
+        }
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error getting all products: {e}")
-
-@product_router.get("/{product_id}", response_model=ProductResponse)
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={
+            "message": f"Internal server error: {e}"
+        })
+        
+@product_router.get("/{product_id}")
 def get_product(product_id: int, db: Session = Depends(get_db)):
     try:
+        product = product_service.get_products(db=db, product_id=product_id)
         
-        return product_service.get_products(db=db, product_id=product_id)
+        if not product:
+            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={
+                "message": "Item not found",
+            })
+        return {
+            "message": "Success retrieved product",
+            "product": product
+        }
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error getting product: {e}")
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={
+            "message": f"Internal Server Error: {e}"
+        })
 
 @product_router.post("/", response_model=ProductResponse)
 def create_product(product_create: ProductCreate, db: Session = Depends(get_db)):
